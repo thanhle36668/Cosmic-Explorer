0fa5a3f1ede660bf80fcbdb0b970b6fa55ee426c
Thành Lê <thanhle36668@gmail.com>
2025-07-10 16:18:14 +0700

    tạo file details-page-planet trong views user

---
+++ Cosmic-Explorer-Project/resources/views/user/details-page-planet.blade.php
@@ -0,0 +1,79 @@
+@extends('layouts.user.details-page')
+
+@section('title')
+    <title>Cosmic Explorer</title>
+@endsection
+
+@section('section-change')
+    <!-- ***** Header ***** -->
+    <header class="header-area header-sticky">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <nav class="main-nav">
+                        <!-- ***** Logo Start ***** -->
+                        <a href="index.html" class="logo">
+                            <img src="{{ asset('images') }}/logo.svg" alt="">
+                        </a>
+                        <!-- ***** Logo End ***** -->
+
+                        <!-- ***** Menu Start ***** -->
+                        <ul class="nav">
+                            <li>
+                                <a href="#">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">About</a>
+                            </li>
+                            <li class="nav-item dropdown planets">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Planets
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    @foreach ($planets as $planet)
+                                        <li>
+                                            <a class="dropdown-item" href="#">{{ $planet->name }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown constellation">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Constellations
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    @foreach ($constellations as $constellation)
+                                        <li>
+                                            <a class="dropdown-item" href="#">{{ $constellation->name }}</a>
+                                        </li>
+                                    @endforeach
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown educational">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Educational
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <a class="dropdown-item" href="#">Videos</a>
+                                    </li>
+                                    <li>
+                                        <a class="dropdown-item" href="#">Books</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <a class='menu-trigger'>
+                            <span>Menu</span>
+                        </a>
+                        <!-- ***** Menu End ***** -->
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- ***** Header End ***** -->
+@endsection
