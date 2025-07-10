e6b99d7ea2478ba8d74d20de5abc4e3267705bde
Thành Lê <thanhle36668@gmail.com>
2025-07-10 16:16:51 +0700

    xóa thẻ title và thêm @yield('title')

--- Cosmic-Explorer-Project/resources/views/layouts/user/details-page.blade.php
+++ Cosmic-Explorer-Project/resources/views/layouts/user/details-page.blade.php
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap"
         rel="stylesheet">
 
-    <title>Cosmic Explorer</title>
+    @yield('title')
 
     <!-- Bootstrap core CSS -->
     <link href="{{ asset('user') }}/bootstrap/css/bootstrap.min.css" rel="stylesheet">
