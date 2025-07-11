b897d9d7f0bb88e3cde57d643da3f2093067f1d0
LinnieNguyen <107024459+LinnieNguyen@users.noreply.github.com>
2025-07-11 11:29:32 +0700

    tạo comment và post cho database

--- Cosmic-Explorer-Project/routes/web.php
+++ Cosmic-Explorer-Project/routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CosmicExplorerController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
 use Illuminate\Support\Facades\Route;
 
 // User
@@ -18,6 +19,7 @@ Route::middleware(['auth', 'is_admin'])
         Route::get('/', fn() => view('admin.dashboard'))
             ->name('admin.dashboard');
         Route::resource('posts', \App\Http\Controllers\Admin\PostController::class);
+        Route::resource('comments', CommentController::class);
     });
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
