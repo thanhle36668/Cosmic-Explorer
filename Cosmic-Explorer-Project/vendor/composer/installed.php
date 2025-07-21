fcc2a42dae0df191ea9e013f7ba07242e4465b1b
NGTRUONGGIANG\giang <giang17012004@gmail.com>
2025-07-21 11:42:13 +0700

    no message

--- Cosmic-Explorer-Project/vendor/composer/installed.php
+++ Cosmic-Explorer-Project/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-main',
-        'version' => 'dev-main',
-        'reference' => 'bfc795e8365532b12d754af1c903e2923e8042cd',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -380,9 +380,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-main',
-            'version' => 'dev-main',
-            'reference' => 'bfc795e8365532b12d754af1c903e2923e8042cd',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
